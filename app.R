9b65dbec5f2452587aafab91a7d9ae6f30599a0d
Shawn Morrison <shawn@dryas.ca>
2018-02-16 13:41:46 -0700

    Add files via upload

--- app.R
+++ app.R
@@ -10,14 +10,15 @@ ui <- fluidPage(
   # Application title
   titlePanel("Mapping Sensitive Species"),
   
-  # Sidebar with a slider input for number of bins 
+  # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      selectInput("species",
-                  "Select a Species:",
-                  c("Caribou", "Burrowing Owl", "Marcus"))
-    ),
-    
+      # selectInput("species",
+      #             "Select a Species:",
+      #             c("Caribou", "Burrowing Owl", "Marcus"))
+    )
+    ,
+
     # Show a plot of the generated distribution
     mainPanel(
       leafletOutput("speciesplot")
@@ -31,26 +32,44 @@ server <- function(input, output) {
   
   alberta <- readOGR("GeoBoundaries", "BF ATS v4_1 Alberta Provincial Boundary")
   
+  # Read in caribou
   Caribou <- readOGR("SpeciesLayers/Caribou", "Caribou_Range")
   Caribou <- spTransform(Caribou, 
                          CRS("+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"))
   
-  #create pallet of colors for caribou ranges 
-  qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
-  col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
-  factpal <- colorFactor(col_vector, Caribou$LOCALRANGE)
+  # Read in burrowing owl  
+  BurrOwl <- readOGR("SpeciesLayers/BurrOwl", "Burrowing_Owl_Range")
+  BurrOwl <- spTransform(BurrOwl, 
+                         CRS("+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"))
   
   #create map
   output$speciesplot <- renderLeaflet({
-    leaflet() %>% 
-      addPolygons(data=alberta, weight=0.5, col = 'black') %>% #Add provincial border
-      addPolygons(data=Caribou, opacity = 1, stroke = FALSE, weight=1) %>% 
-      addTiles(urlTemplate = "https://mts1.google.com/vt/lyrs=s&hl=en&src=app&x={x}&y={y}&z={z}&s=G", attribution = 'Google') %>%
-      addLegend(pal = factpal,
-        values  = Caribou$LOCALRANGE,
-        position = "bottomright",
-        title = "Caribou Range",
-        labFormat = labelFormat(digits=1))
+    leaflet() %>%
+      
+      # Set the location to center and zoom the map
+      setView(lng = -113, lat = 55, zoom = 4) %>%
+    
+      # Base maps
+      addTiles(urlTemplate = "https://mts1.google.com/vt/lyrs=s&hl=en&src=app&x={x}&y={y}&z={z}&s=G", attribution = 'Google', group = "Google") %>%
+      addProviderTiles(providers$Esri.WorldStreetMap, group = "Esri") %>%
+      addMiniMap(
+        tiles = providers$Esri.WorldStreetMap,
+        toggleDisplay = T) %>%
+
+      # Overlay groups
+      addPolygons(data=alberta, weight=2, fillColor = "transparent", stroke = TRUE, color = "black", group = "Alberta") %>%
+      addPolygons(data=Caribou, opacity = 1, color = "blue", stroke = FALSE, weight=1, group = "Caribou") %>%
+      addPolygons(data=BurrOwl, opacity = 1, color = "red", stroke = FALSE, weight=1, group = "Burrowing Owl") %>%
+      hideGroup(c("Caribou", "Burrowing Owl")) %>% # ensures the species are OFF by default
+      
+      
+        # Layers control
+      addLayersControl(
+        baseGroups = c("Google", "Esri"),
+        overlayGroups = c("Caribou", "Burrowing Owl"),
+        options = layersControlOptions(collapsed = FALSE)
+      )
+    
 })
 }
 
